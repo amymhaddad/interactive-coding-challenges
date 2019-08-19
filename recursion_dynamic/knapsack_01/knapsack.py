c6389f483f5acb5b4c7f01b9ee1ff92c6a33e4f1
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-19 07:16:09 -0400

    Working on algorithm

--- recursion_dynamic/knapsack_01/knapsack.py
+++ recursion_dynamic/knapsack_01/knapsack.py
@@ -16,22 +16,44 @@
 total_weight = 8
 items = [['a', 2, 2], ['b', 4, 2], ['c', 6, 4], ['d', 9, 5]]
 
+weight_total = []
 
-for i, row in enumerate(items):
-    print(row[i])
+# for i, item in enumerate(items):
+#     print(item[-1] + item[-1])
+#     # print(item)
+   
+weights = [2, 2, 4, 5]
+item_weight_used = []
 
-#why is this out of range and the example below works just fine?
+# for outer in range(len(weights)):
+#     for inner in range(len(weights)):
+#         added_items = weights[outer] + weights[inner]
+        
+#         if added_items <= total_weight:
+#             weight_total.append(added_items)
+#             # print(weights[inner])
+# #         # item_weight_used.append()
+            
+#     print(max(weight_total))
+# # print(max(weight_total))
 
-arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
 
-d1 = 0
-for i, row in enumerate(arr):
-    d1 += row[i]
-    print(row[i])
+for i, outer in enumerate(items):
+    for j in inner enumerate(items):
+        print(items[-2])
 
 
 
+item_val_wt = {
+    'a': [2, 2], 
+    'b': [4, 2],
+    'c': [6, 4],
+    'd': [9, 5],
+}
 
+# for weight in weights:
+    # print(weight)
+    
 # def knapsack_items(items):
 
 #     knapsack_weight = 0
