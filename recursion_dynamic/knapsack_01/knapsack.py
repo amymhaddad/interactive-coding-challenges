d7491cd41c2f53fbe1dcd4d31d86b2e97727c646
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-20 10:23:54 -0400

    Got the start of the algorightm to work

--- recursion_dynamic/knapsack_01/knapsack.py
+++ recursion_dynamic/knapsack_01/knapsack.py
@@ -1,47 +1,32 @@
 
-# class Item(object):
-
-#     def __init__(self, label, value, weight):
-#         self.label = label
-#         self.value = value
-#         self.weight = weight
-
-#     def __repr__(self):
-#         return self.label + ' v:' + str(self.value) + ' w:' + str(self.weight)
-
-# items = [label, value, wt]
-#Find the items with the MAX val
-
 
 total_weight = 8
 # items = [['a', 2, 2], ['b', 4, 2], ['c', 6, 4], ['d', 9, 5]]
 
-added_weight = []
-items = [2, 2, 4, 5]
+# added_weight = []
+# items = [2, 2, 4, 5]
 
-items_to_extract = []
+# items_to_extract = []
 
-for i, item in enumerate(items):
-    for j, item in enumerate(items):
-        if items[i] + items[j] <= total_weight:
-            added_weight.append(items[i] + items[j])
-    max_wt = max(added_weight)
-    if items[i] + items[j] == max_wt:
-        items_to_extract.append(items[i])
-        items_to_extract.append(items[j])
+# for i, item in enumerate(items):
+#     for j, item in enumerate(items):
+#         if items[i] + items[j] <= total_weight:
+#             added_weight.append(items[i] + items[j])
+#     max_wt = max(added_weight)
+#     if items[i] + items[j] == max_wt:
+#         items_to_extract.append(items[i])
+#         items_to_extract.append(items[j])
 
-# print(items_to_extract)
+item_names = ['a', 'b', 'c', 'd']
+values = [2, 4, 6, 9]
+weights = [2, 2, 4, 5]
 
-# x = [2, 5, 2, 5]
-#I get the correct item weights but I need another conditional so I get the last 2 numbers in the returned sequence based on the values they return. I want the largest value.
+# for item, val, wt in list(zip(item_name, value, weight)):
+#     print(item)
 
-x = [['a', 2, 2], ['b', 4, 2], ['c', 6, 4], ['d', 9, 5]]
+totals = list(zip(item_names, values, weights))
 
-# def find_item(x):
-
-
-
-
-    
-
-
+knapsack_wt = []
+for i, weight in enumerate(weights):
+    for j, wt in enumerate(weights):
+        
